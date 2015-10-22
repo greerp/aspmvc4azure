b0862afa53138860f48034f0e06bf2138fbc5e4a
Paul Greer <paul@greer.uk.com>
2015-10-22 14:39:50 +0100

    Removed web site deployment to see whether Kudu creates a web.config file

--- deploy.cmd
+++ deploy.cmd
@@ -1,10 +1,6 @@
 @echo off
-echo Deploying files...
-rem xcopy %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip %DEPLOYMENT_TEMP% /Y
-echo Uncompressing files...
-rem compact /u %DEPLOYMENT_TEMP%\SimpleMvcWeb-001.zip %DEPLOYMENT_TARGET%
-
-unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
+echo Extracting File System Deployment ...
+rem unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
 
 
 
