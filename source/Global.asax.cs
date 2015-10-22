d71fd90a5d7c701d3ed7507d01210c5464d15843
Paul Greer <paul@greer.uk.com>
2015-10-22 14:08:53 +0100

    Created File System Publish

--- source/Global.asax.cs
+++ source/Global.asax.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using SimpleMvcWeb;
 
-namespace SimpleMvcWeb
+namespace aspmvc4azure.web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
