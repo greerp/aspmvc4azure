9d072d3af98d7311986d9c1cb85918bf975a180c
Paul Greer <paul@greer.uk.com>
2015-10-22 15:06:07 +0100

    no message

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo Extracting File System Deployment ...
-rem unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
+unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
 
 
 
