538a4aa977930b7a59453632ba6891f88ebe14d1
Paul Greer <paul@greer.uk.com>
2015-10-26 16:44:17 +0000

    Created initial PS script to do the deploy

---
+++ deploy.ps1
@@ -0,0 +1,25 @@
+﻿$bacpac = "F:\GitHub\aspmvc4azure\deploy\azuredb.bacpac"
+$sqlpackage="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe"
+$cn = "Data Source=pgsql015.database.windows.net;Initial Catalog=aspmvc;User Id=greepau;Password=R3dpixie;"
+$toks = $cn.split(";")
+
+ForEach ($tok in $toks) { 
+    $nv = $tok.split("=")
+
+    switch ($nv[0].ToLower()){
+        "data source" {$Server=$nv[1]}
+        "initial catalog" {$Database=$nv[1]}
+        "user id" {$User=$nv[1]}
+        "password" {$password=$nv[1]}
+    }
+}
+
+Write-Host "Server: ${Server}"
+Write-Host "Database: ${Database}"
+Write-Host "User: ${User}"
+Write-Host "Password: ${password}"
+
+Write-Host "${sqlpackage}" "/Action:import" "/tsn:${server}" "/tdn:${database}" "/tu:${User} /tp:${password}" "/sf:${bacpac}"
+
+
+
