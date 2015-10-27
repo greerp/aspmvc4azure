c3cfeb193a21c37d5939ba034e9ada73db17ff6f
Paul Greer <paul@greer.uk.com>
2015-10-27 12:00:58 +0000

    Added command to deploy bacpac Updated json to include MS servers to firewall

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,8 @@
 @echo off
 echo Extracting File System Deployment ...
 unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
+"d:\program files (x86)\Microsoft Sql Server\120\bin\DAC\bin\sqlpackage.exe" /action:import /tsn:pgsql015.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE\deploy\azuredb.bacpac
+
 
 
 
