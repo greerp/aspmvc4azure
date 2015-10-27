38c8fb23d554dfc72bd2b69f02cd861310675983
Paul Greer <paul@greer.uk.com>
2015-10-27 15:41:27 +0000

    no message

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,9 @@
 @echo off
 echo Extracting File System Deployment ...
+echo "%DBSERVER% > %HOME%\server.txt
+echo "%APPLICATIONNAME" > %HOME%\app.txt
 unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
-"d:\program files (x86)\Microsoft Sql Server\120\bin\DAC\bin\sqlpackage.exe" /action:import /tsn:pgsql015.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE\deploy\azuredb.bacpac
+"d:\program files (x86)\Microsoft Sql Server\120\DAC\bin\sqlpackage.exe" /action:import /tsn:%DBSERVER%.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE\deploy\azuredb.bacpac
 
 
 
