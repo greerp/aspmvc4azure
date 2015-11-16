9dc8fa7afd3f997f1562804443661c9f173fc64e
Paul Greer <paul@greer.uk.com>
2015-11-16 16:37:19 +0000

    no message

--- deploy.ps1
+++ deploy.ps1
@@ -22,7 +22,7 @@ Write-Host "Password: ${password}"
 #Write-Host "${sqlpackage}" "/Action:import" "/tsn:${server}" "/tdn:${database}" "/tu:${User} /tp:${password}" "/sf:${bacpac}"
 
 
-#sqlpackage /Action:Export /ssn:LDNLT00099\SQLEXPRESS /sdn:azuredb /tf:c:\temp\sql\azuredb.bacpac
+. $sqlpackage /Action:Export /ssn:LDNLT00099\SQLEXPRESS /sdn:udo_angular /tf:c:\temp\sql\azuredb.bacpac
 
 #C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120>sqlpackage /action:import /tsn:pgsql015.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:F:\GitHub\aspmvc4azure\deploy\azuredb.bacpac
 
