e158da121cfcd2a77d3dd199926ec0856dcba0d6
Paul Greer <paul@greer.uk.com>
2015-11-23 13:47:52 +0000

    If the database doesnt exist the deployment fails, assuming this is because the database referenced in the connection string is non existent

--- deploy.cmd
+++ deploy.cmd
@@ -3,4 +3,4 @@ echo ******* Extracting File System Deployment *******
 unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
 
 echo ******* Deploying database from bacpac file *******
-rem "d:\program files (x86)\Microsoft Sql Server\120\DAC\bin\sqlpackage.exe" /action:import /tsn:%DBSERVER%.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE%\deploy\azuredb.bacpac
+"d:\program files (x86)\Microsoft Sql Server\120\DAC\bin\sqlpackage.exe" /action:import /tsn:%DBSERVER%.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE%\deploy\azuredb.bacpac
