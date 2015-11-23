b4adef017f06147ab6c29a656f19b956769ebfea
Paul Greer <paul.greer@redpixie.com>
2015-11-23 11:47:31 +0000

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -3,8 +3,4 @@ echo ******* Extracting File System Deployment *******
 unzip %DEPLOYMENT_SOURCE%\deploy\SimpleMvcWeb-001.zip -d %DEPLOYMENT_TARGET%
 
 echo ******* Deploying database from bacpac file *******
-"d:\program files (x86)\Microsoft Sql Server\120\DAC\bin\sqlpackage.exe" /action:import /tsn:%DBSERVER%.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE%\deploy\azuredb.bacpac
-
-
-
-
+rem "d:\program files (x86)\Microsoft Sql Server\120\DAC\bin\sqlpackage.exe" /action:import /tsn:%DBSERVER%.database.windows.net /tdn:azuredb /tu:greepau /tp:R3dpixie /sf:%DEPLOYMENT_SOURCE%\deploy\azuredb.bacpac
